3a6d40ac85073af919d8917038c2bab6f6dc606a
Domantas <domantasm96@gmail.com>
2017-10-08 22:14:46 +0200

    Code cleanup + jupyter-notebook + latex_report

--- main.py
+++ main.py
@@ -5,35 +5,48 @@ import numpy as np
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from langdetect import detect
-
-
-# nltk.download('punk')
 from sklearn.model_selection import cross_val_predict
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
 
-file = 'URL-categorization_10000.csv'
-counter = 1
+
+def score_calculation(labels, predicition):
+    print(prediction)
+    print("Accuracy score: {}".format(metrics.accuracy_score(labels, prediction)))
+    # print("Precision_score: {}".format(metrics.precision_score(labels, prediction, average='micro')))
+    print("Precision_score: {}".format(metrics.precision_score(labels, prediction, average='micro')))
+    print("Recall_score: {}".format(metrics.recall_score(labels, prediction, average='micro')))
+    print("F1 score: {}".format(metrics.f1_score(labels, prediction, average='micro')))
+    print('Confusion matrix: \n{}'.format(metrics.confusion_matrix(labels, prediction)))
+    print()
+
+file = 'URL-categorization-DFE.csv'
+limiter = 2000
+top = 50
+cv_number = 5
 reader = csv.reader(open(file), delimiter=',')
 header = next(reader)
 char_blacklist = list(chr(i) for i in range(32, 127) if i <= 64 or i >= 91 and i <= 96 or i >= 123)
 stopwords = nltk.corpus.stopwords.words('english')
 stopwords.extend(char_blacklist)
 language_whitelist = ['en']
+domains_whitelist = ['com', 'org', 'net', '.us', '.uk', '.au', '.ca']
 english_vocab = set(w.lower() for w in nltk.corpus.words.words())
-top = 50
-cv_number = 5
+# print(datetime.datetime.now().time())
 data = []
-tokens_list = []
-freq_words = []
-filter_data = []
-labels = []
-print(datetime.datetime.now().time())
-
 for row in reader:
     data.append(row)
 
+tokens_list = []
+filter_data = []
+counter = 0
+print('URL parsing and filtering')
 for url_counter, row in enumerate(data):
+    if url_counter >= limiter:
+        break
     if row[5] != 'Not_working' and float(row[6]) > 0.5:
         try:
             url = 'http://' + row[-1]
@@ -46,17 +59,18 @@ for url_counter, row in enumerate(data):
             chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
             text = '\n'.join(chunk.lower() for chunk in chunks if chunk)
             text_vocab = set(w.lower() for w in text if w.lower().isalpha())
-            if detect(text) in language_whitelist:
+            if detect(text) not in language_whitelist or (row[-1][-3:] not in domains_whitelist and row[-1][-3:] not in domains_whitelist):
                 continue
+            counter += 1
             tokens = nltk.word_tokenize(text)
             tokens_list += [nltk.word_tokenize(text)]
             print('----')
             print('URL: ' + row[-1])
+            print('Lang: '+ detect(text))
             print('CATEGORY: ' + row[5])
             print(url_counter)
             print('Nr: ', counter)
             filter_data += [row[5]]
-            counter += 1
 
         except:
             pass
@@ -64,7 +78,8 @@ print('Filtered English URL')
 f1 = nltk.FreqDist(filter_data).most_common()
 f2 = list(category for category, number in f1 if number >= cv_number)
 all_categories = list(set(f2))
-print('LABELS CREATION')
+print('CREATING LABELS DATA.')
+labels = []
 counter = 0
 for index, word in enumerate(filter_data):
     if word in all_categories:
@@ -74,7 +89,8 @@ for index, word in enumerate(filter_data):
         counter += 1
 save = labels
 labels = np.array(labels).reshape((len(labels), 1))
-print('CREATE FREQUENT WORDS LIST')
+print('CREATING FREQUENT WORDS LIST..')
+freq_words = []
 for tokens in tokens_list:
     allWordDist = nltk.FreqDist(w.lower() for w in tokens)
     allWordExceptStopDist = nltk.FreqDist(
@@ -83,31 +99,34 @@ for tokens in tokens_list:
     mostCommon = allWordExceptStopDist.most_common(top)
     freq_words += [word for word, number in mostCommon]
 
+print('CREATING FEATURES DATA...')
 features = np.zeros(pow(len(tokens_list), 2) * top).reshape(len(tokens_list), len(tokens_list) * top)
-
-print('FEATURES CREATION')
 for index, line in enumerate(tokens_list):
     for word in line:
         if word in freq_words:
             features[index][freq_words.index(word)] = 1
 
-print(labels)
+for number, word in enumerate(all_categories):
+    print(number, word)
 print(labels.shape, features.shape)
+print(features)
 c, r = labels.shape
 labels = labels.reshape(c,)
+print('************ Logistic Regression ************')
 lr = LogisticRegression()
 prediction = cross_val_predict(lr, features, labels, cv=cv_number)
-print(prediction)
-print("Precision_score: {}".format(metrics.precision_score(labels, prediction, average='micro')))
-print("Recall_score: {}".format(metrics.recall_score(labels, prediction, average='micro')))
-print("F1 score: {}".format(metrics.f1_score(labels, prediction, average='micro')))
+score_calculation(labels, prediction)
+print('************ Support Vector Machine ************')
+lr = SVC()
+prediction = cross_val_predict(lr, features, labels, cv=cv_number)
+score_calculation(labels, prediction)
+print('************ Decision Tree ************')
+lr = DecisionTreeClassifier()
+prediction = cross_val_predict(lr, features, labels, cv=cv_number)
+score_calculation(labels, prediction)
+print('************ KNeighbors ************')
+lr = KNeighborsClassifier(n_neighbors=5, metric="euclidean")
+prediction = cross_val_predict(lr, features, labels, cv=cv_number)
+score_calculation(labels, prediction)
 
-# with open("filtered_data_1000", 'a') as o1:
-#     for row in filter_rows:
-#         counter = 0
-#         for word in row:
-#             counter += 1
-#             if counter == len(row):
-#                 o1.write(word)
-#             else: o1.write(word+',')
-#         o1.write('\n')
+print(datetime.datetime.now().time())
