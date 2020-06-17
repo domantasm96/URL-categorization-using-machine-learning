773f1e578fd9b134d7bf3b3f44307be7f03cf973
Domantas <domantasm96@gmail.com>
2020-06-18 01:03:44 +0300

    Updated README

--- construct_data.sh
+++ construct_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Executing construct_features.py"
-python3 construct_features.py
+python3 01_construct_features.py
 echo "Executing construct_models.py"
-python3 construct_models.py
+python3 02_construct_models.py
 echo "Executing train_models.py"
-python3 train_models.py
+python3 03_train_models.py
