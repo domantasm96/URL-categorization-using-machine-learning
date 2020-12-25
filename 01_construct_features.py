bbd800734c480c27e7bbfad12340b76374163312
Domantas <domantasm96@github.com>
2020-12-25 22:45:06 +0200

    Add multi-threading for scrapping

--- 01_construct_features.py
+++ 01_construct_features.py
@@ -1,13 +1,13 @@
 import pandas as pd
-import urllib.request
 import nltk
 import os
 from datetime import datetime
-from urllib.request import urlopen
-from multiprocessing import Pool, cpu_count
+import requests
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
+
 
 def remove_stopwords(tokens):
     for i, token in enumerate(tokens):
@@ -17,58 +17,60 @@ def remove_stopwords(tokens):
     return tokens_lemmatize
 
 
-def crawl(object):
-    print(f"{object['i']}/{len(urls)} || {object['url']}")
-    tokens_lemmatize = ''
+def scrape(url):
+    print(url[0], url[1])
     try:
-        req = urllib.request.Request(object['url'], headers=hdr)
-        html = urlopen(req, timeout=15).read()
-        soup = BeautifulSoup(html, "html.parser")
-        [tag.decompose() for tag in soup("script")]
-        [tag.decompose() for tag in soup("style")]
-        text = soup.get_text()
-        lines = (line.strip() for line in text.splitlines())
-        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-        text = '\n'.join(chunk.lower() for chunk in chunks if chunk)
-        # Tokenize text
-        tokens = [token.lower() for token in word_tokenize(text)]
-        # Remove stopwords
-        tokens_lemmatize = remove_stopwords(tokens)
-    except Exception:
-        print(f"{object['i']}/{len(urls)} || {object['url']} FAILED. ")
-    return tokens_lemmatize if len(tokens_lemmatize) else ''
-    #     return page_tokens[object['i']]
+        return requests.get(url[1], headers=request_headers, timeout=15)
+    except:
+        return ''
 
-date = datetime.now().strftime("%Y-%M-%d")
-input_path = f'Datasets/URL-categorization-DFE.csv'
+
+date = datetime.now().strftime("%Y-%m-%d")
+input_path = f'Datasets/url_categorization_dfe.csv'
 output_path = f'Datasets/Feature_dataset_{date}.csv'
+
+request_headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+    'Accept-Encoding': 'none',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Connection': 'keep-alive'}
+
+wnl = WordNetLemmatizer()
+char_blacklist = list(
+    chr(i) for i in range(32, 127) if (i <= 47 or i >= 58) and (i <= 64 or i >= 91) and (i <= 96 or i >= 123))
+stopwords = nltk.corpus.stopwords.words('english')
+stopwords.extend(char_blacklist)
+
 if not os.path.isfile(output_path):
-    df = pd.read_csv(input_path)[['url', 'main_category', 'main_category:confidence']]
-    df = df[(df['main_category'] != 'Not_working') & (df['main_category:confidence'] >= 0.5)]
-    df['url'] = df['url'].map(lambda x: 'http://' + x)
-
-    hdr = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-        'Accept-Encoding': 'none',
-        'Accept-Language': 'en-US,en;q=0.8',
-        'Connection': 'keep-alive'}
-    wnl = WordNetLemmatizer()
-    char_blacklist = list(
-        chr(i) for i in range(32, 127) if (i <= 47 or i >= 58) and (i <= 64 or i >= 91) and (i <= 96 or i >= 123))
-    stopwords = nltk.corpus.stopwords.words('english')
-    stopwords.extend(char_blacklist)
-
-    start = datetime.now()
-    print(start)
-    urls = [{"i": index, "url": url} for index, url in enumerate(list(df['url'].values))]
+    df = pd.read_csv(input_path)[['url', 'main_category', 'main_category_confidence']]
+    df = df[(df['main_category'] != 'Not_working') & (df['main_category_confidence'] >= 0.5)].reset_index(drop=True)
+    df['url'] = df['url'].apply(lambda x: 'http://' + x)
     df['tokens'] = ''
-    p = Pool(cpu_count() * 2)
-    tokens = p.map(crawl, urls)
-    df['tokens'][:len(tokens)] = tokens
+    with ThreadPoolExecutor(16) as executor:
+        start = datetime.now()
+        print("Scraping begins: ", start)
+        results = executor.map(scrape, [(i, elem) for i, elem in enumerate(df['url'].values)])
     stop = datetime.now()
-    print(stop)
     exec_time = stop - start
-    print(exec_time)
+    print('Scraping finished. Time: ', exec_time)
+
+    for i, res in enumerate(results):
+        if res != '' and res.status_code == 200:
+            soup = BeautifulSoup(res.text, "html.parser")
+            [tag.decompose() for tag in soup("script")]
+            [tag.decompose() for tag in soup("style")]
+            text = soup.get_text()
+            lines = (line.strip() for line in text.splitlines())
+            chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+            text = '\n'.join(chunk.lower() for chunk in chunks if chunk)
+            # Tokenize text
+            tokens = [token.lower() for token in word_tokenize(text)]
+            # Remove stopwords
+            tokens_lemmatize = remove_stopwords(tokens)
+            df.at[i, 'tokens'] = tokens_lemmatize
+        else:
+            df.at[i, 'tokens'] = ['']
+
     df.to_csv(output_path, index=False)
