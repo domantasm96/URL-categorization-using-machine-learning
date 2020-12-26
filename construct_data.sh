74873cdb2f0c6bbc5d1698da84c6a4cd6d3fc615
Domantas <domantasm96@github.com>
2020-12-26 21:01:38 +0200

    Clean file structure
    Jupyter-notebook files removed

--- construct_data.sh
+++ construct_data.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 echo "Executing construct_features.py"
 python3 01_construct_features.py
-echo "Executing construct_models.py"
-python3 02_construct_models.py
-echo "Executing train_models.py"
-python3 03_train_models.py
